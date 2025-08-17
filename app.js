cbcd6392d9bf9f8c1695bcb4531ac05248ba3548
maleee14 <akmalmf14@gmail.com>
2025-08-17 22:35:16 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import connection from "./connection.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "404_NOT_FOUND" });
+});
+
+connection();
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(
+    `Server running at http://${process.env.DB_HOST}:${process.env.APP_PORT}`
+  );
+});
