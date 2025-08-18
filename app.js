09200a054992107c052469b1a1ed6d566e530634
maleee14 <akmalmf14@gmail.com>
2025-08-18 12:15:35 +0800

    create api router

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import connection from "./connection.js";
+import apiRouter from "./routes/api.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(apiRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "404_NOT_FOUND" });
